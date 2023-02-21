0ef84a3df71fa2959b3655c5cd3320e30a535198
reeganrajasekar <areeganrajasekar@gmail.com>
2023-02-21 18:14:08 +0530

    Change

--- about/governing_council.php
+++ about/governing_council.php
@@ -5,24 +5,168 @@
             <article class="border rounded p-3">
                 <h1 class="sjcet-title">Governing Council</h1>
                 <div class="table-responsive mt-3">
-                    <table class="table table-striped sjcet-text table-bordered">
+                    <table class="table table-striped align-middle sjcet-text table-bordered">
                         <thead>
-                            <tr>
+                            <tr style="text-align:center">
                                 <th scope="col">#</th>
-                                <th scope="col">Description</th>
+                                <th scope="col">Name of the Member</th>
                                 <th scope="col">Position</th>
+                                <th scope="col">Category</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
+                                <td class="table-bold">
+                                    Rev. Fr. Dr. J. E. Arulraj<br>
+                                    Founder (DMI Foundations) /<br>
+                                    Dr. V. Ramachandran<br>
+                                    C.E.O
+                                </td>
+                                <td>Chairman</td>
+                                <td>Members of the Management</td>
                             </tr>
                             <tr>
                                 <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
+                                <td class="table-bold">
+                                    Rev. Sr. S. Gnanaselvam<br>
+                                    Managing Trustee
+                                </td>
+                                <td>Member</td>
+                                <td>Members of the Management</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td class="table-bold">
+                                    Dr. M. Suresh<br>
+                                    Director
+                                </td>
+                                <td>Member</td>
+                                <td>Members of the Management</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td class="table-bold">
+                                    Dr. M. A. Leo Vijilious<br>
+                                    Director
+                                </td>
+                                <td>Member</td>
+                                <td>Members of the Management</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">5</th>
+                                <td class="table-bold">
+                                    Rev. Sr. S. Xavier Selvi<br>
+                                    In-charge of Higher Education<br>
+                                    DFT Group of Institutions
+                                </td>
+                                <td>Member</td>
+                                <td>Members of the Management</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">6</th>
+                                <td class="table-bold">
+                                    Rev. Sr. J. Arockia Johnsi Rani<br>
+                                    Administrator
+                                </td>
+                                <td>Member</td>
+                                <td>Members of the Management</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">7</th>
+                                <td class="table-bold">
+                                    Dr. S. Mangairkarasi<br>
+                                    Professor &amp; Head<br>
+                                    Department of Computer Science and Engineering
+                                </td>
+                                <td>Member</td>
+                                <td>Teachers of the College</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">8</th>
+                                <td class="table-bold">
+                                    Mr. P. Sathis Kumar<br>
+                                    Assistant Professor<br>
+                                    Department of Mechanical Engineering
+                                </td>
+                                <td>Member</td>
+                                <td>Teachers of the College</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">9</th>
+                                <td class="table-bold">
+                                    Dr. B. Ramadoss<br>
+                                    Professor (HAG)<br>
+                                    Department of Computer Application<br> 
+                                    NIT Trichy
+                                </td>
+                                <td>Member</td>
+                                <td>Educationist – Management Nominee</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">10</th>
+                                <td class="table-bold">
+                                    Veteran Wing Commander<br>
+                                    Mr. M. Jeyakumar<br>
+                                    Chairman &amp; Managing Director<br>
+                                    nRoot Technologies Pvt. Ltd
+                                    Thanjavur
+                                </td>
+                                <td>Member</td>
+                                <td>Industrialist – Management Nominee</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">11</th>
+                                <td class="table-bold">
+                                    Regional Officer<br>
+                                    Southern Regional Office, AICTE Chennai
+                                </td>
+                                <td>Member</td>
+                                <td>AICTE (Southern Region) Nominee</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">12</th>
+                                <td class="table-bold">
+                                    Dr. I. Muthumani<br>
+                                    Professor<br>
+                                    Department of Electronics &amp;<br>
+                                    Communication Engineering<br>
+                                    Government College of Engineering<br>
+                                    Tirunelveli
+                                </td>
+                                <td>Member</td>
+                                <td>State Government Nominee</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">13</th>
+                                <td class="table-bold">
+                                    Dr. G. Chandramohan<br>
+                                    Professor &amp; Principal<br>
+                                    PSG Institute of Technology and Applied Research
+                                    Avinashi Road, Neelambur<br>
+                                    Sulur, Coimbatore – 641062.
+                                </td>
+                                <td>Member</td>
+                                <td>University Nominee</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">13</th>
+                                <td class="table-bold">
+                                    Dr. D. Shanmugasundaram<br>
+                                    Principal
+                                </td>
+                                <td>Member</td>
+                                <td>Principal of the College</td>
                             </tr>
                         </tbody>
                     </table>
